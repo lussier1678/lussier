4cc8aa840523ae087d231f3c49774cd6b172d128
lussier1678 <lussierscalator@gmail.com>
2026-04-06 20:11:18 +0300

    Initialize index.js for MediaLab hosting

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import fs from "fs";
 import compression from "compression";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -23,20 +22,9 @@ app.use(morgan("dev"));
 app.use(express.static(publicDir));
 
 app.get("*", (req, res) => {
-  const requestPath = String(req.path || "/").replace(/^\/+/, "");
-  const safePath = requestPath.replace(/\\/g, "/");
-  const directFile = path.join(publicDir, safePath);
-  const htmlFile = safePath.endsWith(".html") ? directFile : path.join(publicDir, safePath + ".html");
-  const folderIndex = path.join(publicDir, safePath, "index.html");
-
-  if (safePath && fs.existsSync(directFile) && fs.statSync(directFile).isFile()) {
-    return res.sendFile(directFile);
-  }
-  if (safePath && fs.existsSync(htmlFile) && fs.statSync(htmlFile).isFile()) {
-    return res.sendFile(htmlFile);
-  }
-  if (safePath && fs.existsSync(folderIndex) && fs.statSync(folderIndex).isFile()) {
-    return res.sendFile(folderIndex);
+  const requestPath = String(req.path || "");
+  if (requestPath.endsWith(".html")) {
+    return res.sendFile(path.join(publicDir, requestPath.replace(/^\/+/, "")));
   }
   return res.sendFile(path.join(publicDir, "index.html"));
 });
